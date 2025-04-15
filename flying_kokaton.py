264099cee17808b1cb1f5c09df81f19e5635fb24
c0a2405455 <c0a2405455@edu.teu.ac.jp>
2025-04-15 17:18:16 +0900

    演習1

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -22,15 +22,19 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        kk_rct.move_ip(-1,0)
         key_lst = pg.key.get_pressed()#練習10こうかとんの移動
+        X=0
+        Y=0
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0,-1)
+            Y-=1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0,+1)
+            Y+=1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1,0)
+            X-=1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+1,0)
+            X+=2
+        kk_rct.move_ip(X,Y)
 
         x=tmr%3200
         screen.blit(bg_img, [-x, 0])#練習6背景を右に動かす
