e835dfae287b67d3623b8ed3163179a758351051
c0a2405455 <c0a2405455@edu.teu.ac.jp>
2025-04-15 16:02:06 +0900

    練習8不連続の解消

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img,True,False)#練習8
     kk_img = pg.image.load("fig/3.png") #練習2こうかとん読み込み
     kk_img = pg.transform.flip(kk_img,True,False)
     kk_img = pg.transform.rotozoom(kk_img,10,1.0)
@@ -20,6 +21,7 @@ def main():
 
         x=tmr
         screen.blit(bg_img, [-x, 0])#練習6背景を右に動かす
+        screen.blit(bg_img2, [-x+1600, 0])#練習7背景を増やす
         screen.blit(kk_img,[300,200])#練習4こうかとん表示
         pg.display.update()
         tmr += 1        
